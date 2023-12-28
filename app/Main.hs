b5d9ee5ba5042f5fbcf7fd73e324d6560c4e0fe0
thelissimus <thelissimus@tuta.io>
2023-12-28 12:03:34 +0500

    setup: server

---
+++ app/Main.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import Data.Aeson (FromJSON, ToJSON, encode)
+import Data.List (find)
+import GHC.Generics (Generic)
+import System.Environment (getEnv)
+import Web.Scotty
+
+data User = MkUser {_id :: Int, name :: String, age :: Int}
+  deriving stock (Show, Generic)
+  deriving anyclass (ToJSON)
+
+users :: [User]
+users =
+  [ MkUser{_id = 1, name = "Alice", age = 24}
+  , MkUser{_id = 2, name = "Bob", age = 42}
+  ]
+
+main :: IO ()
+main = do
+  (port :: Int) <- read <$> getEnv "PORT"
+  putStrLn $ "Running on port: " ++ show port
+  scotty port do
+    get "/" do
+      json users
+
+    get "/:id" do
+      _id <- pathParam "id"
+      json . find (\u -> u._id == _id) $ users
